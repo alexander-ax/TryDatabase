1ec6866a425b493afdc72b709b8842be43cd9f5c
Alex Caberto <alexcaberto.ucc@gmail.com>
2023-07-07 06:18:20 +0800

    Third

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello Alexander! For Todays Bidyow try natin i connect ang Database");
+        System.out.println("Second line");
     }
 }
